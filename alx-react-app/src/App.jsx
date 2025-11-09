6e912251ef182a0e0bce09b08d78804a2e191bd0
AdaezeOkafor <princessgeorge093@gmail.com>
2025-11-09 04:47:55 -0800

    correction on footer.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
