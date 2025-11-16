b35c4363e0685b8193cad6d0418673e506731a08
AdaezeOkafor <princessgeorge093@gmail.com>
2025-11-16 11:04:14 -0800

    a simple company website

---
+++ my-company/src/pages/Services.jsx
@@ -0,0 +1,14 @@
+function Services() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Our Services</h1>
+      <ul style={{ listStyleType: 'none', padding: 0 }}>
+        <li>Technology Consulting</li>
+        <li>Market Analysis</li>
+        <li>Product Development</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services;
