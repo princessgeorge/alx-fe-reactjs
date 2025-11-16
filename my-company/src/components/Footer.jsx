d8c646bf49df71414d00c1c9dd497310be0364af
AdaezeOkafor <princessgeorge093@gmail.com>
2025-11-16 10:46:41 -0800

    Add new components

--- my-company/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ textAlign: 'center', padding: '20px', background: '#282c34', color: 'white', marginTop: '20px' }}>
-      &copy; {new Date().getFullYear()} My Company. All rights reserved.
-    </footer>
-  );
-}
-
-export default Footer;
