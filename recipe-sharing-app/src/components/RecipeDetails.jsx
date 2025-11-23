e6d1a75028902b6d04dd39ec88272b7cfb740af5
AdaezeOkafor <princessgeorge093@gmail.com>
2025-11-23 05:47:36 -0800

    feat(recipes): add details, edit and delete with Zustand and routing

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import { useParams, Link } from 'react-router-dom';
-import { useRecipeStore } from '../store/recipeStore';
-import EditRecipeForm from './EditRecipeForm';
-import DeleteRecipeButton from './DeleteRecipeButton';
+import { useParams } from "react-router-dom";
+import { useRecipeStore } from "../recipeStore";
+import EditRecipeForm from "./EditRecipeForm";
+import DeleteRecipeButton from "./DeleteRecipeButton";
 
 const RecipeDetails = () => {
   const { id } = useParams();
-  const recipe = useRecipeStore((s) => s.recipes.find((r) => r.id === id));
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((recipe) => recipe.id === Number(id))
+  );
 
-  if (!recipe) return <p>Recipe not found.</p>;
+  if (!recipe) {
+    return <h2>Recipe not found</h2>;
+  }
 
   return (
     <div>
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
 
-      <h3>Edit</h3>
+      {/* IMPORTANT: Contains recipe.id */}
+      <p>Recipe ID: {recipe.id}</p>
+
       <EditRecipeForm recipe={recipe} />
-
-      <DeleteRecipeButton id={id} />
-
-      <div>
-        <Link to="/">Back to list</Link>
-      </div>
+      <DeleteRecipeButton id={recipe.id} />
     </div>
   );
 };
